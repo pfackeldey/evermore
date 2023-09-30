98ac0d86ce3e451946a48e422005b441f73c3a3b
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2023-09-30 10:57:11 +0200

    Update __init__.py

--- src/dilax/__init__.py
+++ src/dilax/__init__.py
@@ -3,5 +3,13 @@ dilax: Differentiable (binned) likelihoods in JAX.
 """
 from __future__ import annotations
 
-__all__ = ("__version__",)
+__all__ = ("__version__", "__author__")
+
+__author__ = "Peter Fackeldey"
+__email__ = "peter.fackeldey@rwth-aachen.de"
+__copyright__ = "Copyright 2023, Peter Fackeldey"
+__credits__ = ["Peter Fackeldey"]
+__contact__ = "https://github.com/pfackeldey/dilax"
+__license__ = "BSD-3-Clause"
+__status__ = "Development"
 __version__ = "0.1.0"
