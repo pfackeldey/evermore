32e966bb6e2bd9cc00e3885d36ced53deb116c41
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2024-03-06 15:10:49 +0100

    sample: account for non-evm.Parameters in model

--- src/evermore/sample.py
+++ src/evermore/sample.py
@@ -11,7 +11,7 @@ from evermore.util import is_parameter
 def toy_module(module: eqx.Module, key: PRNGKeyArray) -> PyTree[Callable]:
     from evermore import Parameter
 
-    params_tree = eqx.filter(module, is_parameter, is_leaf=is_parameter)
+    params_tree, rest_tree = eqx.partition(module, is_parameter, is_leaf=is_parameter)
     params_structure = jax.tree_util.tree_structure(params_tree)
     n_params = params_structure.num_leaves  # type: ignore[attr-defined]
 
@@ -33,4 +33,10 @@ def toy_module(module: eqx.Module, key: PRNGKeyArray) -> PyTree[Callable]:
         # replace the sampled parameter value and return new parameter
         return eqx.tree_at(lambda p: p.value, param, sampled_param_value)
 
-    return jax.tree_util.tree_map(_sample, params_tree, keys_tree, is_leaf=is_parameter)
+    # sample for each parameter
+    sampled_params_tree = jax.tree_util.tree_map(
+        _sample, params_tree, keys_tree, is_leaf=is_parameter
+    )
+
+    # combine the sampled parameters with the rest of the model and return it
+    return eqx.combine(sampled_params_tree, rest_tree, is_leaf=is_parameter)
