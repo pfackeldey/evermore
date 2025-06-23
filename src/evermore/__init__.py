823a5996b2f9a0ed0e7d4e4e1056b676e94a2a6c
pfackeldey <fackeldey.peter@gmail.com>
2025-06-23 10:16:17 -0400

    auto adjust copyright year

--- src/evermore/__init__.py
+++ src/evermore/__init__.py
@@ -4,9 +4,11 @@ evermore: Differentiable (binned) likelihoods in JAX.
 
 from __future__ import annotations
 
+import datetime
+
 __author__ = "Peter Fackeldey"
 __email__ = "peter.fackeldey@princeton.edu"
-__copyright__ = "Copyright 2023, Peter Fackeldey"
+__copyright__ = f"Copyright {datetime.datetime.now().year}, Peter Fackeldey"
 __credits__ = ["Peter Fackeldey"]
 __contact__ = "https://github.com/pfackeldey/evermore"
 __license__ = "BSD-3-Clause"
