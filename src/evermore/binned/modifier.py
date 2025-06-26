6f680a070c5e617fb1c28ebc0077d72f8ccd67be
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2025-06-26 15:50:14 -0400

    improve tree_stack (#66)
    * improve tree_stack
    
    * use jax.tree_util.Partial instead of functools.partial

--- src/evermore/binned/modifier.py
+++ src/evermore/binned/modifier.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import abc
 from collections.abc import Callable
-from functools import partial
 from typing import TYPE_CHECKING, Protocol, runtime_checkable
 
 import equinox as eqx
@@ -424,18 +423,12 @@ class Compose(ModifierBase):
                 os = stack.offset_and_scale(_hist)
                 return _hist, os
 
-            # if there is only one modifier in the group, we can skip the scan
-            if len(group_mods) == 1:
-                _, os = calc_sf(hist, dynamic_stack, static_stack)
-                scale *= os.scale
-                offset += os.offset
-            else:
-                _, os = jax.lax.scan(
-                    partial(calc_sf, _static_stack=static_stack),
-                    hist,
-                    dynamic_stack,
-                )
-                scale *= jnp.prod(os.scale, axis=0)
-                offset += jnp.sum(os.offset, axis=0)
+            _, os = jax.lax.scan(
+                jax.tree_util.Partial(calc_sf, _static_stack=static_stack),
+                hist,
+                dynamic_stack,
+            )
+            scale *= jnp.prod(os.scale, axis=0)
+            offset += jnp.sum(os.offset, axis=0)
 
         return OffsetAndScale(offset=offset, scale=scale)
